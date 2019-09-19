5ddc845e24486528f5e42845d2e88f5995f610b3
juanep <39977066+juanep@users.noreply.github.com>
2019-09-18 22:12:05 -0300

    Add files via upload

---
+++ Ejercicio_31/Ejercicio_31/Negocio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+    class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        public Cliente Cliente
+        {
+            get { return clientes.Dequeue(); }
+            set { this.clientes.Enqueue(value); }
+        }
+
+        public Negocio() { }
+
+        public Negocio(string nombre) : this() { this.nombre = nombre; }
+    }
+}
---
+++ Ejercicio_31/Ejercicio_31/PuestoAtencion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ejercicio_31
+{
+    class PuestoAtencion
+    {
+        private static int numeroActual;
+        private Puesto puesto;
+
+        public static int NumeroActual
+        {
+            get { return ++numeroActual; }
+        }
+
+        public static void service()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                Thread.Sleep(1000); //dormir este proceso durante 1000ms
+                Console.WriteLine(x);
+            }
+        }
+
+        public bool Atender(Cliente cliente)
+        {
+            bool isOnCall = true;
+            string numero = cliente.Numero.ToString();
+            var onCall = new Thread(PuestoAtencion.service);
+            onCall.Name = numero;
+            Console.WriteLine("Client Service to " + cliente.Nombre);
+            onCall.Start();
+            if (!onCall.IsAlive) { isOnCall = false; }
+            return isOnCall;
+        }
+
+        private PuestoAtencion() { numeroActual = 0; }
+
+        public PuestoAtencion(Puesto puesto) : this() { this.puesto = puesto; }
+
+        public enum Puesto { Caja1, Caja2 }
+    }
+}
